726f032ff9d53092c439383bbd0db8e31f22a8eb
Gaurav Pratap Singh <gs492979@gmail.com>
2025-05-29 23:46:08 +0530

    minor bug fixed.

--- src/huggingFaceAi.js
+++ src/huggingFaceAi.js
@@ -3,7 +3,6 @@ const SYSTEM_PROMPT = `
 You are an assistant that receives a list of ingredients that a user has and suggests a recipe they could make with some or all of those ingredients. You don't need to use every ingredient they mention in your recipe. The recipe can include additional ingredients they didn't mention, but try not to include too many extra ingredients. Format your response in markdown to make it easier to render to a web page
 `;
 const VITE_HF_API_ACCESS_TOKEN = import.meta.env.VITE_HF_ACCESS_TOKEN;
-console.log(VITE_HF_API_ACCESS_TOKEN);
 
 // for HF_ACCESS_TOKEN
 const hf = new InferenceClient(VITE_HF_API_ACCESS_TOKEN);
